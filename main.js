76cb10e391b57beca2e3992f60d655680e1c7b60
shahdelshamy <134215710+shahdelshamy@users.noreply.github.com>
2024-11-28 02:25:50 +0200

    First Update

--- main.js
+++ main.js
@@ -1,16 +1,49 @@
-// import to  express library
-const express = require("express") ;
+const express = require("express");
+const ws = require('ws');
 
-// create an instance of express
-const instance = express() ;
+// Create an instance of express
+const instance = express();
+const port = 9090;
+const server = instance.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
-const port=9090;
+// WebSocket server setup
+const webSocket = new ws.Server({ server });
 
-instance.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+let data=null;
 
- //get(route, params)
-instance.get("/",(request,response)=>{
-    response.send("Server responsed!")
-})  
+webSocket.on('connection', (socket) => {
+    console.log('WebSocket connected on server side');
+
+    if (data) {
+        socket.send(JSON.stringify(data));
+    }
+
+    socket.on('message', (message) => {
+         data = JSON.parse(message);
+
+        webSocket.clients.forEach((client) => {
+            if (client.readyState === ws.OPEN) {
+                client.send(JSON.stringify({
+                    name: data.name,
+                    age: data.age,
+                    gender: data.gender,
+                    skills: data.skills
+                }));
+            } else {
+                console.error("Error: client not open");
+            }
+        });
+
+        console.log('Received message:', message.toString());
+    });
+});
+
+// Serve static files (HTML, JS, etc.)
+instance.use(express.static(__dirname + "/public"));
+
+// Serve the HTML page at root
+instance.get("/", (request, response) => {
+    response.sendFile(__dirname + "/index.html");
+});
